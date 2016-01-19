93e4c2183ce89cb74b0e1a9edf27d2aa8bff5b65
Konstantin Walter <koalmawa1197@gmail.com>
2016-01-19 18:28:17 +0100

    Added Colors, some fixes in Code for Colors

--- src/PicSort_About.java
+++ src/PicSort_About.java
@@ -1,45 +1,102 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 public class PicSort_About extends JFrame {
     int pd = 16;
-    int useW = 240;
-    int finalW = useW + 2 * pd;
+    int finalW = 240;
     int currentY = 0;
+    Color hd = new Color(0xBF360C);
+    Color bg = new Color(0x263238);
+    Color fg = new Color(0x455A64);
+    Color cClick = new Color(0x69F0AE);
     Container cp = getContentPane();
     public PicSort_About(String title) {
         super(title);
-        addHeaderPanel("About", new Color(0x1A237E), Color.WHITE, 30);
-        addColorPanel("Made by Konstantin Walter", new Color(0x0D47A1), Color.WHITE);
-        addColorPanel("Version 0.01.2.A", new Color(0x263238), Color.WHITE);
-        addColorPanel("18th January 2016", new Color(0x263238), Color.WHITE);
+        addHeaderPanel("About", hd, Color.WHITE, 30);
+        addPadding(pd);
+        addColorPanel("Made by Konstantin Walter", new Color(0x455A64), Color.WHITE);
+        addClickPanel("Open GitHub Page", cClick, Color.BLACK, 30, 0);
+        addPadding(pd);
+        addColorPanel("Version 0.01.2.A", fg, Color.WHITE);
+        addColorPanel("18th January 2016", fg, Color.WHITE);
+        addClickPanel("Open Project's GitHub Page", cClick, Color.BLACK, 30, 1);
+        addPadding(pd);
         createWindow();
     }
-    public void addHeaderPanel(String text, Color bg, Color fg, int fontSize) {
+    public void addHeaderPanel(String text, Color back, Color fg, int fontSize) {
         JLabel l = new JLabel(text, SwingConstants.CENTER);
         l.setForeground(fg);
         l.setFont(new Font("Header", Font.PLAIN, fontSize));
         JPanel headerPanel = new JPanel(new GridLayout());
-        headerPanel.setBounds(0, currentY, finalW, 60);
-        headerPanel.setBackground(bg);
+        headerPanel.setBounds(0, currentY, finalW, 80);
+        headerPanel.setBackground(back);
         headerPanel.add(l);
         a(headerPanel, 0);
     }
-    public void addColorPanel(String text, Color bg, Color fg) {
+    public void addColorPanel(String text, Color back, Color fore) {
         JLabel l = new JLabel(text, SwingConstants.CENTER);
-        l.setForeground(fg);
+        l.setForeground(fore);
         l.setFont(new Font("Header", Font.PLAIN, 12));
         JPanel panel = new JPanel(new GridLayout());
         panel.setBounds(0, currentY, finalW, 30);
-        panel.setBackground(bg);
+        panel.setBackground(back);
         panel.add(l);
         a(panel, 0);
     }
-    public void a(Component c, int pd) {
+    public void addClickPanel(String text, Color back, Color fore, int height, int method) {
+        JLabel l = new JLabel(text, SwingConstants.CENTER);
+        l.setForeground(fore);
+        l.setFont(new Font("label", Font.PLAIN, 12));
+        JPanel panel = new JPanel(new GridLayout());
+        panel.setBounds(0, currentY, finalW, height);
+        panel.setBackground(back);
+        panel.add(l);
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                switch (method) {
+                    case 0:
+                        openWebsite("https://github.com/pickoala");
+                        break;
+                    case 1:
+                        openWebsite("https://github.com/pickoala/PicSort");
+                    default:
+                        break;
+                }
+            }
+        });
+        a(panel, 0);
+    }
+    public void addPadding(int height) {
+        JPanel padding = new JPanel();
+        padding.setBounds(0, currentY, finalW, height);
+        padding.setBackground(bg);
+        a(padding, 0);
+    }
+    public void openWebsite(String url) {
+        switch (System.getProperty("os.name").toLowerCase()) {
+            case "mac os x":
+                Runtime rt = Runtime.getRuntime();
+                try {
+                    rt.exec( "open " + url);
+                } catch(IOException e) {
+                    System.out.println("Runtime::" + e);
+                }
+                break;
+            default:
+                break;
+
+        }
+        System.out.println(System.getProperty("os.name").toLowerCase());
+    }
+    public void a(Component c, int padding) {
         cp.add(c);
-        plus(c.getHeight() + pd);
+        plus(c.getHeight() + padding);
     }
     public void plus(int add) { currentY += add;}
     public void createWindow() {
--- src/PicSort_Welcome.java
+++ src/PicSort_Welcome.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 /**
  * Version 0.01.2.A
  * created on 18th January 2016
@@ -13,40 +15,60 @@ public class PicSort_Welcome extends JFrame {
     int useW = 300;
     int finalW = useW + 2 * pd;
     int currentY = 0;
+    Color hd = new Color(0xBF360C);
+    Color bg = new Color(0x263238);
+    Color cClick = new Color(0x69F0AE);
     Container cp = getContentPane();
     public PicSort_Welcome(String title) {
         super(title);
-        addHeaderPanel("PicSort", new Color(0x5caf50), Color.WHITE, 30);
-        addButton("Start PicSort", ae -> startPicSort());
-        addButton("About", ae -> openAbout());
+        addHeaderPanel("PicSort", hd, Color.WHITE, 30);
+        addPadding(pd);
+        addClickPanel("Start PicSort", cClick, Color.BLACK, 40, 0);
+        addPadding(pd);
+        addClickPanel("About", cClick, Color.BLACK, 30, 1);
+        addPadding(pd);
         createWindow();
     }
-    public void addHeaderPanel(String text, Color bg, Color fg, int fontSize) {
+    public void addHeaderPanel(String text, Color back, Color fore, int fontSize) {
         JLabel l = new JLabel(text, SwingConstants.CENTER);
-        l.setForeground(fg);
+        l.setForeground(fore);
         l.setFont(new Font("Header", Font.PLAIN, fontSize));
         JPanel headerPanel = new JPanel(new GridLayout());
         headerPanel.setBounds(0, currentY, finalW, 60);
-        headerPanel.setBackground(bg);
+        headerPanel.setBackground(back);
         headerPanel.add(l);
-        a(headerPanel, 2 * pd);
+        a(headerPanel, 0);
     }
-    public void addButton(String text, ActionListener ae) {
-        JButton b = new JButton();
-        b.setBounds(pd, currentY, useW, 24);
-        b.setText(text);
-        b.addActionListener(ae);
-        a(b, pd);
-    }
-    public void addClickPanel(String text, Color bg, Color fg) {
+    public void addClickPanel(String text, Color back, Color fore, int height, int method) {
         JLabel l = new JLabel(text, SwingConstants.CENTER);
-        l.setForeground(fg);
-        l.setFont(new Font("Header", Font.PLAIN, 12));
+        l.setForeground(fore);
+        l.setFont(new Font("label", Font.PLAIN, 12));
         JPanel panel = new JPanel(new GridLayout());
-        panel.setBounds(0, currentY, finalW, 30);
-        panel.setBackground(bg);
+        panel.setBounds(0, currentY, finalW, height);
+        panel.setBackground(back);
         panel.add(l);
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                switch (method) {
+                    case 0:
+                        startPicSort();
+                        break;
+                    case 1:
+                        openAbout();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
         a(panel, 0);
+    }
+    public void addPadding(int height) {
+        JPanel padding = new JPanel();
+        padding.setBounds(0, currentY, finalW, height);
+        padding.setBackground(bg);
+        a(padding, 0);
     }
     public void startPicSort() {
         dispose();
@@ -63,7 +85,7 @@ public class PicSort_Welcome extends JFrame {
     public void plus(int add) { currentY += add;}
     public void createWindow() {
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setSize(finalW, currentY + 34);
+        setSize(finalW, currentY + 22);
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation( (d.width - getWidth()) / 2, (d.height - getHeight()) / 2);
         setResizable(false);
