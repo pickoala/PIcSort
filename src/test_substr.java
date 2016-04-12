13365e7f9d0b19df9c179765aa51269761a0f4b8
Konstantin Walter <koalmawa1197@gmail.com>
2016-04-12 19:33:47 +0200

    For some weeks final, still no possibilities of showing pictures

--- src/test_substr.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by konstantin on 21/01/16.
- */
-public class test_substr {
-    static String colorStr = "100";
-    public static void main(String[] args) {
-        System.out.println("Value of \\”" + colorStr + "\\”:" + Integer.valueOf(colorStr, 15));
-    }
-}
